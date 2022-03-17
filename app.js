c46555132e41a2b52fdfdd5c27b0f752b39c01e7
Euichang Park <euichangpark@gmail.com>
2022-03-17 19:21:29 -0400

    feature22

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 const json = require('./data.json');
-
 const action = process.argv[2];
+const note = process.argv[3];
+let { nextEntryId: nextEntryId } = jason.notes;
+nextEntryId = json.nextId;
 
 if (action === 'read') {
-  console.log(JSON.stringify(json, null, 2));
+  console.log(data);
+} else if (action === 'add') {
+  json.notes.nextEntryId = note;
+  json.nextId++;
+  const data = JSON.stringify(json, null, 2);
+  fs.writeFile('data.json', `${data}`, 'utf8', err => {
+    if (err) throw err;
+  })
 }
