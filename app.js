4fd9f3fc0a6c53107913d9eca968b478a9ebb324
dcatan89 <danieljcatan@gmail.com>
2022-03-17 23:53:08 +0000

    feature 3 done

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const fs = require('fs');
 const json = require('./data.json');
 const action = process.argv[2];
 const note = process.argv[3];
-let { nextEntryId: nextEntryId } = jason.notes;
-nextEntryId = json.nextId;
+const update = process.argv[4];
+const read = JSON.stringify(json, null, 2);
 
 if (action === 'read') {
-  console.log(data);
+  console.log(read);
 } else if (action === 'add') {
-  json.notes.nextEntryId = note;
-  json.nextId++;
-  const data = JSON.stringify(json, null, 2);
-  fs.writeFile('data.json', `${data}`, 'utf8', err => {
+  json.notes[json.nextId++] = note;
+  fs.writeFile('data.json', `${JSON.stringify(json, null, 2)
+}`, 'utf8', err => {
     if (err) throw err;
-  })
+  });
+} else if (action === 'update') {
+  json.notes[note] = update;
+  fs.writeFile('data.json', `${JSON.stringify(json, null, 2)
+}`, 'utf8', err => {
+    if (err) throw err;
+  });
 }
